554a0a5ad91dcc729807ac658f3bda3cb15e8048
Hitesh <me@hiteshjoshi.com>
2017-01-07 23:00:02 +0530

    cron.v2 as inbuilt functionality

---
+++ clock/clock.go
@@ -0,0 +1,145 @@
+package clock
+
+import (
+	"fmt"
+	"log"
+	"net"
+	"net/http"
+	"strings"
+	"time"
+
+	"strconv"
+
+	"os"
+
+	"gopkg.in/redis.v5"
+)
+
+const (
+	USERAGENT       = "Pandat API"
+	TIMEOUT         = 30
+	CONNECT_TIMEOUT = 5
+)
+
+type fn func() string
+type Clock struct {
+	Cron  *Cron
+	Boot  fn
+	Redis *redis.Client
+}
+
+func forever() {
+	for {
+	}
+}
+func New() *Clock {
+
+	client := redis.NewClient(&redis.Options{
+		Addr: os.Getenv("REDIS"),
+	})
+	pong, err := client.Ping().Result()
+
+	fmt.Println("PING TO REDIS, GOT : ", pong, "and ERRORS : ", err)
+
+	c := NewCron()
+
+	e := client.Get("entries")
+	s, _ := e.Bytes()
+	id, _ := strconv.Atoi(string(s))
+	c.NextID = EntryID(id)
+
+	clock := Clock{
+		Cron:  c,
+		Redis: client,
+	}
+
+	//To make it run forever for slaves of clock
+	clock.Boot = func() string {
+		done := make(chan bool)
+		go forever()
+		<-done
+		return ""
+	}
+
+	c.Start() //start cron.v2
+
+	return &clock
+}
+
+//Add : Add new job
+func (c *Clock) Add(interval string, url string) string {
+
+	id, _ := c.Cron.AddJob(interval, Request{
+		URL: url,
+	})
+
+	ID := fmt.Sprint(id)
+
+	//save interval
+	c.Redis.HSet("id:"+ID, "interval", interval)
+
+	//save url
+	c.Redis.HSet("id:"+ID, "url", url)
+
+	c.Redis.Set("entries", ID, 0)
+
+	return ID
+
+}
+
+//Remove a job
+func (c *Clock) Remove(id string) error {
+
+	ID, _ := strconv.Atoi(id)
+
+	c.Cron.Remove(EntryID(ID))
+
+	//remvove from redis
+	e := c.Redis.HDel("id:" + id)
+
+	return e.Err()
+
+}
+
+//This function controls what to run on cron execution
+type Request struct {
+	URL string
+}
+
+func (d Request) Exec(ch chan<- string) {
+	start := time.Now()
+
+	var netTransport = &http.Transport{
+		Dial: (&net.Dialer{
+			Timeout: 5 * time.Second,
+		}).Dial,
+		TLSHandshakeTimeout: 5 * time.Second,
+	}
+	var netClient = &http.Client{
+		Timeout:   time.Second * 10,
+		Transport: netTransport,
+	}
+
+	req, err := http.NewRequest("POST", d.URL, strings.NewReader("data"))
+	resp, err := netClient.Do(req)
+	req.Close = true
+	resp.Close = true
+	defer resp.Body.Close()
+
+	if err != nil {
+		//TODO create an error for this event
+		log.Print("Unable to create a new http request", err)
+	}
+
+	//save status in db
+	log.Printf((d.URL)+" %s", resp.Status)
+
+	secs := time.Since(start).Seconds()
+
+	ch <- fmt.Sprintf("%.2f elapsed with response length: %d %s", secs, d.URL)
+
+}
+func (d Request) Run() {
+	ch := make(chan string)
+	go d.Exec(ch)
+}
--- clock/cron.go
+++ clock/cron.go
@@ -1,146 +1,225 @@
+// Package cron implements a cron spec parser and runner.
 package clock
 
 import (
-	"fmt"
-	"log"
-	"net"
-	"net/http"
-	"strings"
+	"sort"
 	"time"
-
-	"strconv"
-
-	"os"
-
-	cron "gopkg.in/hiteshjoshi/cron.v2"
-	"gopkg.in/redis.v5"
 )
 
-const (
-	USERAGENT       = "Pandat API"
-	TIMEOUT         = 30
-	CONNECT_TIMEOUT = 5
-)
-
-type fn func() string
-type Clock struct {
-	Cron  *cron.Cron
-	Boot  fn
-	Redis *redis.Client
+// Cron keeps track of any number of entries, invoking the associated func as
+// specified by the schedule. It may be started, stopped, and the entries may
+// be inspected while running.
+type Cron struct {
+	entries  []*Entry
+	stop     chan struct{}
+	add      chan *Entry
+	remove   chan EntryID
+	snapshot chan []Entry
+	running  bool
+	NextID   EntryID
 }
 
-func forever() {
-	for {
-	}
-}
-func New() *Clock {
-
-	client := redis.NewClient(&redis.Options{
-		Addr: os.Getenv("REDIS"),
-	})
-	pong, err := client.Ping().Result()
-
-	fmt.Println("PING TO REDIS, GOT : ", pong, "and ERRORS : ", err)
-
-	c := cron.New()
-
-	e := client.Get("entries")
-	s, _ := e.Bytes()
-	id, _ := strconv.Atoi(string(s))
-	c.NextID = cron.EntryID(id)
-
-	clock := Clock{
-		Cron:  c,
-		Redis: client,
-	}
-
-	//To make it run forever for slaves of clock
-	clock.Boot = func() string {
-		done := make(chan bool)
-		go forever()
-		<-done
-		return ""
-	}
-
-	c.Start() //start cron.v2
-
-	return &clock
+// Job is an interface for submitted cron jobs.
+type Job interface {
+	Run()
 }
 
-type Job struct {
-	URL string
+// Schedule describes a job's duty cycle.
+type Schedule interface {
+	// Next returns the next activation time, later than the given time.
+	// Next is invoked initially, and then each time the job is run.
+	Next(time.Time) time.Time
 }
 
-func (d Job) Request(ch chan<- string) {
-	start := time.Now()
+// EntryID identifies an entry within a Cron instance
+type EntryID int
 
-	var netTransport = &http.Transport{
-		Dial: (&net.Dialer{
-			Timeout: 5 * time.Second,
-		}).Dial,
-		TLSHandshakeTimeout: 5 * time.Second,
+// Entry consists of a schedule and the func to execute on that schedule.
+type Entry struct {
+	// ID is the cron-assigned ID of this entry, which may be used to look up a
+	// snapshot or remove it.
+	ID EntryID
+
+	// Schedule on which this job should be run.
+	Schedule Schedule
+
+	// Next time the job will run, or the zero time if Cron has not been
+	// started or this entry's schedule is unsatisfiable
+	Next time.Time
+
+	// Prev is the last time this job was run, or the zero time if never.
+	Prev time.Time
+
+	URL Request
+}
+
+// Valid returns true if this is not the zero entry.
+func (e Entry) Valid() bool { return e.ID != 0 }
+
+// byTime is a wrapper for sorting the entry array by time
+// (with zero time at the end).
+type byTime []*Entry
+
+func (s byTime) Len() int      { return len(s) }
+func (s byTime) Swap(i, j int) { s[i], s[j] = s[j], s[i] }
+func (s byTime) Less(i, j int) bool {
+	// Two zero times should return false.
+	// Otherwise, zero is "greater" than any other time.
+	// (To sort it at the end of the list.)
+	if s[i].Next.IsZero() {
+		return false
 	}
-	var netClient = &http.Client{
-		Timeout:   time.Second * 10,
-		Transport: netTransport,
+	if s[j].Next.IsZero() {
+		return true
 	}
+	return s[i].Next.Before(s[j].Next)
+}
 
-	req, err := http.NewRequest("POST", d.URL, strings.NewReader("data"))
-	resp, err := netClient.Do(req)
-	req.Close = true
-	resp.Close = true
-	defer resp.Body.Close()
+// New returns a new Cron job runner.
+func NewCron() *Cron {
+	return &Cron{
+		entries:  nil,
+		add:      make(chan *Entry),
+		stop:     make(chan struct{}),
+		snapshot: make(chan []Entry),
+		remove:   make(chan EntryID),
+		running:  false,
+	}
+}
 
+// AddJob adds a Job to the Cron to be run on the given schedule.
+func (c *Cron) AddJob(spec string, URL Request) (EntryID, error) {
+	schedule, err := Parse(spec)
 	if err != nil {
-		//TODO create an error for this event
-		log.Print("Unable to create a new http request", err)
+		return 0, err
+	}
+	return c.Schedule(schedule, URL), nil
+}
+
+// Schedule adds a Job to the Cron to be run on the given schedule.
+func (c *Cron) Schedule(schedule Schedule, URL Request) EntryID {
+	c.NextID++
+	entry := &Entry{
+		ID:       c.NextID,
+		Schedule: schedule,
+		URL:      URL,
+	}
+	if !c.running {
+		c.entries = append(c.entries, entry)
+	} else {
+		c.add <- entry
+	}
+	return entry.ID
+}
+
+// Entries returns a snapshot of the cron entries.
+func (c *Cron) Entries() []Entry {
+	if c.running {
+		c.snapshot <- nil
+		return <-c.snapshot
+	}
+	return c.entrySnapshot()
+}
+
+// Entry returns a snapshot of the given entry, or nil if it couldn't be found.
+func (c *Cron) Entry(id EntryID) Entry {
+	for _, entry := range c.Entries() {
+		if id == entry.ID {
+			return entry
+		}
+	}
+	return Entry{}
+}
+
+// Remove an entry from being run in the future.
+func (c *Cron) Remove(id EntryID) {
+	if c.running {
+		c.remove <- id
+	} else {
+		c.removeEntry(id)
+	}
+}
+
+// Start the cron scheduler in its own go-routine.
+func (c *Cron) Start() {
+	c.running = true
+	go c.run()
+}
+
+// run the scheduler.. this is private just due to the need to synchronize
+// access to the 'running' state variable.
+func (c *Cron) run() {
+	// Figure out the next activation times for each entry.
+	now := time.Now().Local()
+	for _, entry := range c.entries {
+		entry.Next = entry.Schedule.Next(now)
 	}
 
-	//save status in db
-	log.Printf((d.URL)+" %s", resp.Status)
+	for {
+		// Determine the next entry to run.
+		sort.Sort(byTime(c.entries))
 
-	secs := time.Since(start).Seconds()
+		var effective time.Time
+		if len(c.entries) == 0 || c.entries[0].Next.IsZero() {
+			// If there are no entries yet, just sleep - it still handles new entries
+			// and stop requests.
+			effective = now.AddDate(10, 0, 0)
+		} else {
+			effective = c.entries[0].Next
+		}
 
-	ch <- fmt.Sprintf("%.2f elapsed with response length: %d %s", secs, d.URL)
+		select {
+		case now = <-time.After(effective.Sub(now)):
+			// Run every entry whose next time was this effective time.
+			for _, e := range c.entries {
+				if e.Next != effective {
+					break
+				}
+				go e.URL.Run()
+				e.Prev = e.Next
+				e.Next = e.Schedule.Next(effective)
+			}
+			continue
 
+		case newEntry := <-c.add:
+			c.entries = append(c.entries, newEntry)
+			newEntry.Next = newEntry.Schedule.Next(now)
+
+		case <-c.snapshot:
+			c.snapshot <- c.entrySnapshot()
+
+		case id := <-c.remove:
+			c.removeEntry(id)
+
+		case <-c.stop:
+			return
+		}
+
+		now = time.Now().Local()
+	}
 }
-func (d Job) Run() {
-	ch := make(chan string)
-	go d.Request(ch)
 
+// Stop the cron scheduler.
+func (c *Cron) Stop() {
+	c.stop <- struct{}{}
+	c.running = false
 }
 
-//Add : Add new job
-func (c *Clock) Add(interval string, url string) string {
-
-	id, _ := c.Cron.AddJob(interval, Job{
-		URL: url,
-	})
-
-	ID := fmt.Sprint(id)
-
-	//save interval
-	c.Redis.HSet("id:"+ID, "interval", interval)
-
-	//save url
-	c.Redis.HSet("id:"+ID, "url", url)
-
-	c.Redis.Set("entries", ID, 0)
-
-	return ID
-
+// entrySnapshot returns a copy of the current cron entry list.
+func (c *Cron) entrySnapshot() []Entry {
+	var entries = make([]Entry, len(c.entries))
+	for i, e := range c.entries {
+		entries[i] = *e
+	}
+	return entries
 }
 
-//Remove a job
-func (c *Clock) Remove(id string) error {
-
-	ID, _ := strconv.Atoi(id)
-
-	c.Cron.Remove(cron.EntryID(ID))
-
-	//remvove from redis
-	e := c.Redis.HDel("id:" + id)
-
-	return e.Err()
-
+func (c *Cron) removeEntry(id EntryID) {
+	var entries []*Entry
+	for _, e := range c.entries {
+		if e.ID != id {
+			entries = append(entries, e)
+		}
+	}
+	c.entries = entries
 }
